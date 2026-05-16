71570f886bd81594295f1854047414ec5d0e8e76
SUYASH <sahusuyash237@gmail.com>
2026-05-16 13:00:41 +0530

    practicing comparasion and conditional functions, d05_p03.py

---
+++ month-01/d05_p03(conditionalANDcomparasion function practice).py
@@ -0,0 +1,14 @@
+# Practicing number comparasion and conditional functions :--
+print()
+x=int(input("What's x "))
+y=int(input("What's y "))
+
+if x > y:
+    print("x is greater than y")
+    print()
+elif x < y:
+    print("x is smaller than y")
+    print()
+else:
+    print("x is equal to y")
+    print()
