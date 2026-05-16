520398d43940a40412f84c6b34a46342ddf3e8a0
SUYASH <sahusuyash237@gmail.com>
2026-05-16 12:54:19 +0530

    made a positive and negative checker'd05_p02'

---
+++ month-01/d05_p02(positiveNnegative checker).py
@@ -0,0 +1,15 @@
+# CHECKING IF AN INTEGER IS POSITIVE NEGATIVE OR ZERO :--
+
+print()
+a = int(input("Please input an integer: "))
+print()
+
+if a > 0:
+    print("Given integer is positive.")
+    print()
+elif a< 0:
+    print("Given integer is negative.")
+    print()
+else:
+    print("Give integer is zero.")
+    print()
